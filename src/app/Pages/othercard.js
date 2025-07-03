57838280d974d27487bf8fd00e21de7a3f2d4f9f
Netik Pareek <pareeknetik48@gmail.com>
2025-07-03 17:17:29 +0530

    onemore

--- src/app/Pages/othercard.js
+++ src/app/Pages/othercard.js
@@ -2,22 +2,25 @@ import { DirectionAwareHover2 } from "@/components/cards-2-2";
 import { DirectionAwareHover } from "@/components/ui/hoverCard";
 
 export function DirectionAwareHoverDemo2() {
-  const image =
-  "../../public/train2.jpeg";
+  const image = "/train2.jpeg"; // Public folder path doesn't need `../../public`
+
   return (
-    (<div className="h-[40rem] relative  flex items-center justify-center pr-0">
+    <div className="relative flex flex-col md:flex-row items-center justify-center gap-6 md:h-[40rem] px-4 py-8">
       <DirectionAwareHover2 image={image}>
-      <p className="font-bold text-xl">Surendra Singh</p>
-      <p className="font-normal text-sm"> Male Coach</p>
-      <p className="font-normal text-sm"> 5th dan karate BLack belt, International refree, National Grappling Coach </p>
+        <p className="font-bold text-xl">Surendra Singh</p>
+        <p className="font-normal text-sm">Male Coach</p>
+        <p className="font-normal text-sm">
+          5th dan karate Black belt, International referee, National Grappling Coach
+        </p>
       </DirectionAwareHover2>
-      <DirectionAwareHover image={image} className={"ml-4"}>
+
+      <DirectionAwareHover image={image} className="md:ml-4">
         <p className="font-bold text-xl">Deepa Gupta</p>
-        <p className="font-normal text-sm"> Female Coach</p>
-        <p className="font-normal text-sm"> 2nd dan karate BLack belt, National Grappling refree </p>
+        <p className="font-normal text-sm">Female Coach</p>
+        <p className="font-normal text-sm">
+          2nd dan karate Black belt, National Grappling referee
+        </p>
       </DirectionAwareHover>
-      
-      
-    </div>)
+    </div>
   );
 }
